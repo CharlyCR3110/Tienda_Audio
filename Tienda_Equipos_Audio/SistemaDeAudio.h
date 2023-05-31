a9d9236fbf546f66bd9d0b163ac64625a7f3969e
charly <carlosgarita3110@gmail.com>
2023-05-30 20:26:19 -0600

    refactor: use _nombreComponente(Componente attribute) instead of _nombre local attr

--- Tienda_Equipos_Audio/SistemaDeAudio.h
+++ Tienda_Equipos_Audio/SistemaDeAudio.h
@@ -13,8 +13,6 @@ public:
 	SistemaDeAudio(const SistemaDeAudio& other);
 	virtual ~SistemaDeAudio();
 	virtual double getPrecio() const override;
-	virtual std::string getNombre() const override;
-	virtual void setNombre(std::string nombre);
 	virtual std::string toString() const override;
 	// metodo clonar
 	virtual Componente* clonar() const override;
@@ -30,5 +28,4 @@ private:
 	int _cantidad;
 	Componente** _componentes;
 	//void resize(int n);
-	std::string _nombre;
 };
