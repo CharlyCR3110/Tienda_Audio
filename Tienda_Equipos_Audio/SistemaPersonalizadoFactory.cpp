36116bafaa5232b7c25c2caac17144c05f615e70
charly <carlosgarita3110@gmail.com>
2023-05-31 18:04:09 -0600

    refactor: Renamed TiendaFisicaFactory to SistemaPersonalizadoFactory

---
+++ Tienda_Equipos_Audio/SistemaPersonalizadoFactory.cpp
@@ -0,0 +1,37 @@
+﻿#include "SistemaPersonalizadoFactory.h"
+
+Componente* SistemaPersonalizadoFactory::crearProcesadorDeSenal()
+{
+    return ICrearProductos::crearProcesadorDeSenal();
+}
+
+Componente* SistemaPersonalizadoFactory::crearFuenteDeAudio()
+{
+    return ICrearProductos::crearFuenteDeAudio();
+}
+
+Componente* SistemaPersonalizadoFactory::crearParlanteCat()
+{
+    return ICrearProductos::crearParlanteCat();
+}
+
+Componente* SistemaPersonalizadoFactory::crearSistemaDeAudio()
+{
+    // Un sistema de audio tiene un procesador de senal, una fuente de audio y un parlante
+
+    // el codigo debe de ser una cadena string de 5 numeros aleatorios
+    srand(time(NULL));
+    std::string codigo = "";
+    for (int i = 0; i < 5; i++)
+    {
+        int num = rand() % 10;
+        char c = num + 48;
+        codigo += c;
+    }
+
+    Componente* sistemaDeAudio = new SistemaDeAudio(codigo, ICrearProductos::mensajeNombreSistemaDeAudio(), "Sistema Personalizado");
+    sistemaDeAudio->add(crearProcesadorDeSenal());
+    sistemaDeAudio->add(crearFuenteDeAudio());
+    sistemaDeAudio->add(crearParlanteCat());
+    return sistemaDeAudio;
+}
