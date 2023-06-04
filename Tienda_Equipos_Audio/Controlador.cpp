832475015f89b93904700d84222e20e195a8858c
charly <carlosgarita3110@gmail.com>
2023-06-04 17:56:29 -0600

    feat(menuVentaDirecta): add controladorVentaDirecta and controladorMenuVentaDirectaComprar methods

--- Tienda_Equipos_Audio/Controlador.cpp
+++ Tienda_Equipos_Audio/Controlador.cpp
@@ -18,7 +18,7 @@ void Controlador::controladorPrincipal(int opcion)
 	switch (opcion)
 	{
 	case 1:
-		Interfaz::menuVentaDirecta();
+		controladorVentaDirecta();
 		std::system("pause");
 		break;
 	case 2:
@@ -38,6 +38,105 @@ void Controlador::controladorPrincipal(int opcion)
 		Interfaz::opcionInvalida();
 		break;
 	}
+}
+
+void Controlador::controladorVentaDirecta()
+{
+	Cliente* cliente = nullptr;
+	try
+	{
+		cliente = Interfaz::buscarCliente();
+		std::cout << "Bienvenido " << cliente->getNombre() << std::endl;
+		std::system("pause");
+		clearScreen();
+	}
+	catch (std::exception& e)
+	{
+		std::stringstream mensajeDeError;
+		mensajeDeError << "Error. El cliente parece no estar suscrito. Motivo: " << e.what() << std::endl;
+		mensajeDeError << "Por favor, registre al cliente antes de continuar." << std::endl;
+		std::cerr << mensajeDeError.str() << std::endl;
+		std::system("pause");
+		clearScreen();
+		return;
+	}
+
+	int unidades = 0;
+	Componente* componenteActual = nullptr;
+	char seguirComprando = 's';
+
+	Venta* venta = new VentaDirecta(cliente, Interfaz::tienda->getFechaActual());
+
+	do
+	{
+		try
+		{
+			componenteActual = controladorMenuVentaDirectaComprar();
+			do
+			{
+				std::cout << "Cuantas unidades desea" << std::endl;
+				std::cin >> unidades;
+			} while (unidades < 0);
+			venta->agregarComponente(componenteActual, unidades);
+		}
+		catch (std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+			std::system("pause");
+		}
+
+		do
+		{
+			std::cout << "Desea seguir comprando? (s/n): ";
+			std::cin >> seguirComprando;
+		} while (seguirComprando != 's' && seguirComprando != 'S' && seguirComprando != 'n' && seguirComprando != 'N');
+	} while (seguirComprando == 's' || seguirComprando == 'S');
+
+	clearScreen();
+	std::cout << "----------------------------------------------------------------------------" << std::endl;
+	try
+	{
+		std::cout << venta->generarFactura() << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		std::system("pause");
+	}
+	std::cout << "----------------------------------------------------------------------------" << std::endl;
+	try
+	{
+		Interfaz::tienda->agregarVenta(venta);
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		std::system("pause");
+	}
+}
+
+Componente* Controlador::controladorMenuVentaDirectaComprar()
+{
+	int opcionMenuVentaDirecta = Interfaz::obtenerOpcionMenuVentaDirecta();
+	Componente* componente = nullptr;
+
+	switch (opcionMenuVentaDirecta)
+	{
+	case 1:
+		componente = Interfaz::crearSistemaPersonalizado();
+		break;
+	case 2:
+		componente = Interfaz::escogerSistemaPreconfigurado();
+		break;
+	case 3:
+		componente = Interfaz::escogerComponenteSeparado();
+		break;
+	default:
+		throw std::exception("Menu Venta Directa: Opcion invalida");
+		break;
+	}
+
+	return componente;
 }
 
 void Controlador::controladorMantenimiento()
--- Tienda_Equipos_Audio/Controlador.h
+++ Tienda_Equipos_Audio/Controlador.h
@@ -8,6 +8,13 @@ public:
 
 	void controladorPrincipal(int opcion);
 
+	// Separando el menuVentaDirecta en partes
+	void controladorVentaDirecta();
+
+	Componente* controladorMenuVentaDirectaComprar();
+
+
+
 	void controladorMantenimiento();
 
 	void controladorReportes(); // en construccion
